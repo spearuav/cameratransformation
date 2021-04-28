2df5ea8072ed2c7f1ca1a555907620b47938a073
Eden Ben Asulin <eden@spearuav.com>
2021-04-28 16:21:46 +0300

    Added GeoLocationTest

--- CameraTransformation.h
+++ CameraTransformation.h
@@ -33,7 +33,7 @@ class CameraTransformation{
   
   CameraTransformation();
   
-  //Setters for model parameters
+  // Setters for model parameters
   
    void setBodyOrientation(double yaw, double pitch, double roll);
    void setGimbalOrientation(double yaw, double pitch, double roll);
@@ -42,13 +42,13 @@ class CameraTransformation{
    void setCurrentAltitude(double alt);
   
   
-  //Getters for model results - world-axes position of an image pixel
+  // Getters for model results - world-axes position of an image pixel
   
    void getPixelDirection(double xraw, double yraw, double &v1, double &v2, double &v3);
    void getPixelVector(double xraw, double yraw, double &x, double &y, double &z);
    void getPixelVectorPolar(double x_raw, double y_raw, double &radius, double &angle);
 
-   //Getters for model results - pixel position of a real-world
+   // Getters for model results - pixel position of a real-world
 
    bool getPointPixel(double v1, double v2, double v3, double &xpoint, double &ypoint);
 	
@@ -56,34 +56,34 @@ class CameraTransformation{
 
   const double Z_EPSILON = 0.001;
 
-  //Body angles. All radians.
+  // Body angles. All radians.
   
   double bodyYaw = 0;
   double bodyPitch = 0;
   double bodyRoll = 0;
   
   
-  //Gimbal angles. All radians.
+  // Gimbal angles. All radians.
   double gimbalYawRad   = 0;
   double gimbalPitchRad = 0;
   double gimbalRollRad  = 0;
     
-  //Camera and sensor information
+  // Camera and sensor information
 
-  double sensorW = 1280;    //Pixels on sensor
-  double sensorH = 720;     //Pxels on sensor
-  double wFovRad = 30;
-  double focalLength = 1;             //Focal length normalized in sensor pixels.
-  double pixsize = 2.9e-6;
+  double sensorW = 1280;    // Pixels on sensor
+  double sensorH = 720;     // Pxels on sensor
+  double wFovRad = 30;      // Vertical feild of view in radians #### doesnt make sense in radians 
+  double focalLength = 1;   // Focal length normalized in sensor pixels.
+  double pixsize = 2.9e-6;  
     
   //Video window information
 
-  double windowW = 640; //Pixels on window
-  double windowH = 480; //Pixels on window
+  double windowW = 640;    // Pixels on window (may change)
+  double windowH = 480;    // Pixels on window(may change)
     
   //World information
   
-  double height=50;             //ASL height estimate. Meters, but can be anything really.
+  double height=50;             //Above sea level height estimate. Meters, but can be anything really.
   
   //Internal geometric representations
   
---
+++ GeoLocationTest.cpp
@@ -0,0 +1,71 @@
+#include "opencv2\highgui.hpp"
+#include "opencv2\core.hpp"
+#include "opencv2\imgproc.hpp"
+#include <iostream>
+#include <iomanip>
+#include "CameraTransformation.h"
+#include "CustomVideoCoorToCoor.h"
+
+using namespace cv;
+using namespace std;
+
+#define window "LiveFeed"
+Mat frame;
+const double camerawFovRads = (55.0 / 180) * 3.1415925358979;
+const double cameraPixelSize = 2.9e-6;
+const double cameraWpixels = 640;
+const double cameraHpixels = 480;
+const double gimbalPitchRad = -0.34906585;
+const double currentAltitude = 2.1;
+const double bodyYawRad = 4.712; // In radians
+const double bodyPitchRads = 0;
+const double bodyRollRads = 0;
+const double currentLatitude = 32.14376;
+const double currentongitude = 34.791120;
+
+CustomVideoCoorToCoor myCam;
+QGeoCoordinate referenceLocation;
+QGeoCoordinate newLocation;
+
+vector<Point> pts;
+
+void drawClickedPoints(Mat img)
+{
+
+	for (size_t i = 0; i < pts.size(); i++)
+	{
+		newLocation = myCam.videoCoorToCoor(0, gimbalPitchRad, 0, bodyYawRad, bodyPitchRads, bodyRollRads, currentAltitude, cameraWpixels, cameraHpixels, pts[i].x, pts[i].y, referenceLocation);
+		circle(img, pts[i], 4, Scalar(255, 0, 0), 2);
+		putText(frame, format("Latitude: %f , Longitude: %f", newLocation.latitude(), newLocation.longitude()), Point(pts[i].x + 7, pts[i].y), FONT_HERSHEY_PLAIN, 0.8, Scalar(230, 140, 70), 1.2);
+	}
+
+}
+void cbmouse(int event, int x, int y, int flags, void*userdata)
+{
+	if (event == EVENT_LBUTTONDOWN)
+	{
+		pts.push_back(Point(x, y));
+	}
+}
+
+int main(int argc, char*argv[])
+{
+	referenceLocation.setLatitude(currentLatitude); // in decimal degrees
+	referenceLocation.setLongitude(currentongitude); // in decimal degrees
+	referenceLocation.setAltitude(currentAltitude); //in meters
+
+	myCam.setCameraParameters(cameraWpixels, cameraHpixels, camerawFovRads);
+
+	VideoCapture video(1);
+	namedWindow(window);
+	setMouseCallback(window, cbmouse, NULL);
+
+	while (1)
+	{
+		video >> frame;
+		drawClickedPoints(frame);
+		imshow(window, frame);
+		if (waitKey(33) == 27)
+			break;
+	}
+}
