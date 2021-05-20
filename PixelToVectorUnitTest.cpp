ea348664ebd0c4e78c7b2165d75cfea5a2864f0e
Eden Ben Asulin <eden@spearuav.com>
2021-05-20 18:10:19 +0300

    Check failed test

--- PixelToVectorUnitTest.cpp
+++ PixelToVectorUnitTest.cpp
@@ -193,6 +193,7 @@ TEST(CameraTransformationTest, CheckRadiusAdnAngleMatch)
 		// Assert outputs are equal with precision of 0.001
 		ASSERT_TRUE(abs(CTRadius - expactedRadius) < PRECISION);
 		ASSERT_TRUE(abs(CTTheta - expactedTheta) < PRECISION);
+		ASSERT_TRUE(0);
 
 	}
 }
