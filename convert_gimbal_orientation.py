Fix rotation inaccuracy

convert_gimbal_orientation.py
@@ -17,18 +17,18 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 
 
-def convert_gimbal_orientation_to_world(body_yaw, body_pitch, body_roll, gimbal_yaw, gimbal_pitch, gimbal_roll,
+def convert_gimbal_orientation_to_world(body_roll, body_pitch, body_yaw, gimbal_roll, gimbal_pitch, gimbal_yaw,
                                         is_degrees=True):
-    Rw_b = R.from_rotvec(np.array([body_yaw, body_pitch, body_roll]), degrees=is_degrees)
-    Rb_g = R.from_rotvec(np.array([gimbal_yaw, -gimbal_pitch, gimbal_roll]), degrees=is_degrees)
+    Rw_b = R.from_euler('zxy', np.array([body_roll, body_pitch, body_yaw]), degrees=is_degrees)
+    Rb_g = R.from_euler('zxy', np.array([gimbal_roll, gimbal_pitch, gimbal_yaw]), degrees=is_degrees)
     Rw_g = Rw_b * Rb_g
 
     return Rw_g.as_euler('zxy')
 
 
 if __name__ == '__main__':
-    drone_body_yaw, drone_body_pitch, drone_body_roll = np.pi/2, 0, 0
-    drone_gimbal_yaw, drone_gimbal_pitch, drone_gimbal_roll = 0, -np.pi/6, 0
+    drone_body_roll, drone_body_pitch, drone_body_yaw = np.pi/2, 0, 0
+    drone_gimbal_roll, drone_gimbal_pitch, drone_gimbal_yaw = 0, -np.pi/6, 0
 
     vector = convert_gimbal_orientation_to_world(drone_body_roll, drone_body_pitch, drone_body_yaw,
                                                  drone_gimbal_roll, drone_gimbal_pitch, drone_gimbal_yaw,

tests/test_gimbal_to_world.py
@@ -14,12 +14,12 @@ class GimbalToWorldTest(unittest.TestCase):
             reader = csv.DictReader(test_file)
             for row in reader:
                 print(row)
-                vector = convert_gimbal_orientation_to_world(float(row['body_yaw_deg']),
+                vector = convert_gimbal_orientation_to_world(float(row['body_roll_deg']),
                                                              float(row['body_pitch_deg']),
-                                                             float(row['body_roll_deg']),
-                                                             float(row['gimbal_yaw_body']),
+                                                             float(row['body_yaw_deg']),
+                                                             float(row['gimbal_roll_body']),
                                                              float(row['gimbal_pitch_body']),
-                                                             float(row['gimbal_roll_body']))
+                                                             float(row['gimbal_yaw_body']))
                 print(np.around(np.degrees(vector), 2))
 
         self.assertEqual(True, True)  # add assertion here